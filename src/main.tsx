d353894ea9822ced9de2c216be805a85b5741a09
Adaamss <aademhamdi@gmail.com>
2026-07-06 12:15:35 +0100

    refactor: update main.tsx to wire up new components

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { FlightForm } from "./components/flightForm/FlightForm.tsx";
-import { PassengerForm } from "./components/passengerForm/PassengerForm .tsx";
+import { PassengerForm } from "./components/passengerForm/PassengerForm.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { NotFound } from "./components/notFound/NotFound.tsx";
+import { Counter } from "./components/counter/counter.tsx";
 
 const router = createBrowserRouter([
   { path: "/flight", element: <FlightForm /> },
+  { path: "/counter", element: <Counter /> },
   { path: "/passenger", element: <PassengerForm /> },
   { path: "*", element: <NotFound /> },
 ]);
