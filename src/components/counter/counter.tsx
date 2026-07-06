f18117e8f9d27b69db5b5a62e9b84c7ff48e6c21
Adaamss <aademhamdi@gmail.com>
2026-07-06 14:32:46 +0100

    refactor: remove old useState logic, keep useReducer implementation

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,36 +1,8 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 import { counterReducer, intialState } from "./reducer";
 import { DECREMENT, INCREMENT, RESET } from "./action";
 
 export const Counter = () => {
-  //   const [counter, setCounter] = useState(0);
-  //   const [history, setHistory] = useState<number[]>([]);
-
-  //   const handlePlus = () => {
-  //     setCounter((prev) => prev + 1);
-  //     setHistory([...history, counter]);
-  //   };
-  //   const handleMinus = () => {
-  //     setHistory([...history, counter]);
-
-  //     setCounter((prev) => {
-  //       if (prev > 0) {
-  //         return prev - 1;
-  //       } else {
-  //         return 0;
-  //       }
-  //     });
-  //   };
-
-  //   const handleReset = () => {
-  //     setCounter(0);
-  //     setHistory([]);
-  //   };
-
-  //   const handleDisabled = () => {
-  //     if (counter > 9) return true;
-  //   };
-
   const [state, dispatch] = useReducer(counterReducer, intialState);
   const handlePlus = () => {
     dispatch(INCREMENT);
@@ -52,15 +24,6 @@ export const Counter = () => {
       <br />
       <button onClick={handleReset}>reset</button>
       <div>{state.history}</div>
-
-      {/* <button disabled={handleDisabled()} onClick={handlePlus}>
-        +
-      </button>
-      <div>{counter}</div>
-      <button onClick={handleMinus}>-</button>
-      <br />
-      <button onClick={handleReset}>reset</button>
-      <div>{history}</div> */}
     </>
   );
 };
