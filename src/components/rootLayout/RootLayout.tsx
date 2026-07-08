b505da888a7e22c8e92231e633175612360a8958
Adaamss <aademhamdi@gmail.com>
2026-07-08 12:58:57 +0100

    feat: style RootLayout with styled-components

--- src/components/rootLayout/RootLayout.tsx
+++ src/components/rootLayout/RootLayout.tsx
@@ -1,12 +1,22 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 import { Header } from "../header/Header";
 import { Footer } from "../footer/Footer";
+
+const Main = styled.main`
+  min-height: 70vh;
+  padding: 24px;
+  max-width: 800px;
+  margin: 0 auto;
+`;
 
 export const RootLayout = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <Main>
+        <Outlet />
+      </Main>
       <Footer />
     </>
   );
