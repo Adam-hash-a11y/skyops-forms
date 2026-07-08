9e3a33bfbfd4d2fe70e5d55de78adcb766a69b0f
Adaamss <aademhamdi@gmail.com>
2026-07-08 12:58:40 +0100

    feat: style Header with styled-components

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,41 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  padding: 16px 24px;
+  background-color: #0f172a;
+`;
+
+const Title = styled.h2`
+  color: white;
+  margin: 0;
+  margin-right: 20px;
+  font-size: 18px;
+`;
+
+const StyledLink = styled(Link)`
+  color: #cbd5e1;
+  text-decoration: none;
+  font-size: 14px;
+  font-weight: 500;
+
+  &:hover {
+    color: white;
+  }
+`;
 
 export const Header = () => {
   return (
-    <>
-      <h2>Hello I'm the header</h2>
-      <Link to="/"> Home</Link>
-      <Link to="/flight">Flight Form</Link>
-      <Link to="/passenger">Passenger Form</Link>
-      <Link to="/counter">Counter </Link>
-    </>
+    <Nav>
+      <Title>SkyOps</Title>
+      <StyledLink to="/">Home</StyledLink>
+      <StyledLink to="/flight">Flight Form</StyledLink>
+      <StyledLink to="/passenger">Passenger Form</StyledLink>
+      <StyledLink to="/flights">Flights List</StyledLink>
+      <StyledLink to="/counter">Counter</StyledLink>
+    </Nav>
   );
 };
