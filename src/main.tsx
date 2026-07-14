51af82b4de66819112e6d88cd2ffb24bb70e5016
Adaamss <aademhamdi@gmail.com>
2026-07-14 13:02:50 +0100

    chore: fix PassengerForm import path

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { FlightForm } from "./components/flightForm/FlightForm.tsx";
-import { PassengerForm } from "./components/passengerForm/PassengerForm .tsx";
+import { PassengerForm } from "./components/passengerForm/PassengerForm.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { NotFound } from "./components/notFound/NotFound.tsx";
 
